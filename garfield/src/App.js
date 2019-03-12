6b8dfa796f3aba36c54bd596888801bc0ce8cf37
Tommy Wisaeu <kingdomkeygaming@gmail.com>
2019-03-12 15:41:28 -0500

    Added more

--- garfield/src/App.js
+++ garfield/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container';
+import ScrollAnimation from 'react-animate-on-scroll';
+import Fade from 'react-reveal/Fade';
 
 class MainCarousel extends Component {
   render() {
@@ -36,10 +38,37 @@ class MainCarousel extends Component {
   }
 }
 
+class CarouselArea extends Component {
+  render() {
+    return (
+      <Fade>
+        <div className="carousel-container">
+          <Container>
+            <h2>Here are said web things</h2>
+            <MainCarousel />
+          </Container>
+        </div>
+      </Fade>
+    )
+  }
+}
+
 class Name extends Component {
   render() {
     return (
-      <div className="name">I do web things</div>
+      <Fade><div className="name">I do web things</div></Fade>
+    )
+  }
+}
+
+class Media extends Component {
+  render() {
+    return (
+      <Fade>
+        <div className="media">
+          <h2>Find me at none of these</h2>
+        </div>
+      </Fade>
     )
   }
 }
@@ -49,12 +78,8 @@ class App extends Component {
     return (
         <div>
           <Name />
-          <div className="carousel-container">
-            <Container>
-              <MainCarousel />
-            </Container>
-          </div>
-
+          <CarouselArea />
+          <Media />
         </div>
     );
   }
